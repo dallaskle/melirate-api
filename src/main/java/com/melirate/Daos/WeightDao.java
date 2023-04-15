1ed6a1787ac2211905629fc2e25decbe7dd10c1e
Dallas Klein <dallasjklein@gmail.com>
2023-04-15 16:31:56 +0200

    WeightDao

---
+++ src/main/java/com/melirate/Daos/WeightDao.java
@@ -0,0 +1,40 @@
+package com.melirate.Daos;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.melirate.models.Weight;
+
+import java.util.List;
+
+public class WeightDao {
+
+    public WeightDao() {
+    }
+
+    public Weight loadWeight(Weight _weight) {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client);
+        Weight weight = dynamoDBMapper.load(Weight.class, _weight.getUserId(), _weight.getTimestamp());
+        return weight;
+    }
+
+    public List<Weight> loadAllWeights(String userId) {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client);
+        Weight weight = new Weight();
+        weight.setUserId(userId);
+        DynamoDBQueryExpression<Weight> queryExpression = new DynamoDBQueryExpression<Weight>()
+                .withHashKeyValues(weight);
+        List<Weight> weights = dynamoDBMapper.query(Weight.class, queryExpression);
+        return weights;
+    }
+
+    public Weight saveWeight(Weight weight) {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client);
+        dynamoDBMapper.save(weight);
+        return weight;
+    }
+}
--- src/main/java/com/melirate/models/Image.java
+++ src/main/java/com/melirate/models/Image.java
@@ -13,7 +13,6 @@ public class Image {
     private String view;
 
     public Image() {
-
     }
 
     @DynamoDBHashKey(attributeName = "user_id")
