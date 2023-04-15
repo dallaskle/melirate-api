c062e54c934baa61b4d8a2c228aa485365845d9a
Dallas Klein <dallasjklein@gmail.com>
2023-04-15 17:50:03 +0200

    lambdas and api gateway work for WeightDAO

--- src/main/java/com/melirate/activity/CreateWeightActivity.java
+++ src/main/java/com/melirate/activity/CreateWeightActivity.java
@@ -5,15 +5,20 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.melirate.dynamodb.DAOs.WeightDao;
 import com.melirate.models.Weight;
 
-public class CreateWeightActivity implements RequestHandler<Weight, String>{
+public class CreateWeightActivity implements RequestHandler<Weight, Weight>{
 
     @Override
-    public String handleRequest(Weight weight, Context context) {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client);
-        dynamoDBMapper.save(weight);
-        return weight.toString();
+    public Weight handleRequest(Weight weight, Context context) {
+        WeightDao weightDao = new WeightDao();
+        try {
+            weightDao.saveWeight(weight);
+        } catch (Exception e) {
+            System.out.println("Exception!");
+            return new Weight();
+        }
+        return weight;
     }
 }
